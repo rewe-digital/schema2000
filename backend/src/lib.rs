b25c0f806c6ef1c0f2295bca87d780aff36a4f4d
Patrick Thaele <patrick.thaele@paymenttools.com>
2021-09-28 13:35:00 +0200

    Create NodeType

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,12 +1,33 @@
 use serde_json::Value;
+use std::iter::Map;
 
 #[derive(Debug)]
-pub struct SchemaHypothesis {}
+pub struct SchemaHypothesis {
+    root: NodeType,
+}
+
+#[derive(Debug)]
+struct Node {
+    node_type: NodeType,
+    required: bool,
+}
+
+#[derive(Debug)]
+enum NodeType {
+    String,
+    Integer,
+    Number,
+    Boolean,
+    // Null,
+    Array(Box<NodeType>), // ["a", "b", "c"] or [1,2,3,4]
+    Object { properties: Map<String, Box<Node>> },
+    // Enum/AnyOf
+}
 
 pub fn generate_hypothesis(_dom: &Value) -> SchemaHypothesis {
-    SchemaHypothesis {}
+    SchemaHypothesis {root: NodeType::String}
 }
 
 pub fn merge_hypothesis(_a: SchemaHypothesis, _b: SchemaHypothesis) -> SchemaHypothesis {
-    SchemaHypothesis {}
+    _a
 }
