def52dd710478df7b0babc3d8bacdd3ce8ba6d61
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2021-09-28 17:29:04 +0200

    analyse and merge schema hypothesis
    Arrays are not done yet.
    
    Co-authored-by: Elmar Athmer <elmar@athmer.org>
    Co-authored-by: lukas prediger <lukas.prediger@rewe-digital.com>
    Co-authored-by: Patrick Thaele <patrick.thaele@paymenttools.com>
    Co-authored-by: Mischa Dieterle <mischa.dieterle@rewe-digital.com>
    Co-authored-by: christoph.baudson <christoph.baudson@rewe-digital.com>

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,33 +1,127 @@
-use serde_json::Value;
-use std::iter::Map;
+pub use merge::merge_hypothesis;
+use serde_json::{Map, Value};
+use std::collections::HashMap;
 
-#[derive(Debug)]
+mod merge;
+
+#[derive(Debug, PartialEq)]
 pub struct SchemaHypothesis {
     root: NodeType,
 }
 
-#[derive(Debug)]
-struct Node {
+#[derive(Clone, Debug, PartialEq)]
+pub struct ObjectProperty {
     node_type: NodeType,
     required: bool,
 }
 
-#[derive(Debug)]
-enum NodeType {
+#[derive(Clone, Debug, PartialEq)]
+pub enum NodeType {
     String,
     Integer,
     Number,
     Boolean,
-    // Null,
-    Array(Box<NodeType>), // ["a", "b", "c"] or [1,2,3,4]
-    Object { properties: Map<String, Box<Node>> },
-    // Enum/AnyOf
+    Null,
+    Array(Box<NodeType>),
+    Object {
+        properties: HashMap<String, ObjectProperty>,
+    },
+    // TODO Use a HashSet. Problem: Neither HashSet nor HashMap implement the Hash trait
+    Any(Vec<NodeType>),
 }
 
-pub fn generate_hypothesis(_dom: &Value) -> SchemaHypothesis {
-    SchemaHypothesis {root: NodeType::String}
+fn generate_properties(properties: &Map<String, Value>) -> HashMap<String, ObjectProperty> {
+    properties
+        .iter()
+        .map(|(key, value)| {
+            (
+                key.to_owned(),
+                ObjectProperty {
+                    required: true,
+                    node_type: generate_node_type(value),
+                },
+            )
+        })
+        .collect()
 }
 
-pub fn merge_hypothesis(_a: SchemaHypothesis, _b: SchemaHypothesis) -> SchemaHypothesis {
-    _a
+fn generate_node_type(dom: &Value) -> NodeType {
+    match dom {
+        Value::Null => NodeType::Null,
+        Value::Bool(_) => NodeType::Boolean,
+        Value::Number(i) if i.is_f64() => NodeType::Number,
+        Value::Number(_) => NodeType::Integer,
+        Value::String(_) => NodeType::String,
+        // TODO: not done yet
+        Value::Array(_) => NodeType::Array(Box::new(NodeType::Null)),
+        Value::Object(props) => NodeType::Object {
+            properties: generate_properties(props),
+        },
+    }
+}
+
+pub fn generate_hypothesis(dom: &Value) -> SchemaHypothesis {
+    SchemaHypothesis {
+        root: generate_node_type(dom),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{generate_node_type, NodeType, ObjectProperty};
+    use maplit::hashmap;
+    use serde_json::json;
+
+    #[test]
+    fn test_null() {
+        let dom = json!(null);
+        assert_eq!(generate_node_type(&dom), NodeType::Null);
+    }
+
+    #[test]
+    fn test_bool() {
+        let dom = json!(true);
+        assert_eq!(generate_node_type(&dom), NodeType::Boolean);
+    }
+
+    #[test]
+    fn test_integer() {
+        let dom = json!(10);
+        assert_eq!(generate_node_type(&dom), NodeType::Integer);
+    }
+
+    #[test]
+    fn test_number() {
+        let dom = json!(10.5);
+        assert_eq!(generate_node_type(&dom), NodeType::Number);
+    }
+
+    #[test]
+    fn test_string() {
+        let dom = json!("Schema 2000");
+        assert_eq!(generate_node_type(&dom), NodeType::String);
+    }
+
+    // #[test]
+    // fn test_array() {
+    //     let dom = json!([10, 15, 25]);
+    //     assert_eq!(generate_node_type(&dom), NodeType::Array(Box::new(NodeType::Integer)));
+    // }
+
+    #[test]
+    fn test_object() {
+        let dom = json!({
+            "name": "Schokoladenbrunnen",
+            "length": 100
+        });
+        assert_eq!(
+            generate_node_type(&dom),
+            NodeType::Object {
+                properties: hashmap! {
+                    "name".to_string() => ObjectProperty{ required: true, node_type: NodeType::String },
+                    "length".to_string() => ObjectProperty{ required: true, node_type: NodeType::Integer }
+                }
+            }
+        );
+    }
 }
---
+++ backend/src/merge.rs
@@ -0,0 +1,216 @@
+use std::collections::HashSet;
+
+use crate::NodeType::Any;
+use crate::SchemaHypothesis;
+use crate::{NodeType, ObjectProperty};
+
+pub fn merge_hypothesis(a: SchemaHypothesis, b: SchemaHypothesis) -> SchemaHypothesis {
+    let root = merge_node_type(a.root, b.root);
+    SchemaHypothesis { root }
+}
+
+fn merge_node_type(a: NodeType, b: NodeType) -> NodeType {
+    match (a, b) {
+        (a, b) if a == b => a,
+        (
+            NodeType::Object {
+                properties: properties_a,
+            },
+            NodeType::Object {
+                properties: properties_b,
+            },
+        ) => {
+            let keys_a: HashSet<&String> = properties_a.keys().collect();
+            let keys_b: HashSet<&String> = properties_b.keys().collect();
+            let merged_properties = keys_a
+                .union(&keys_b)
+                .map(|key| {
+                    (
+                        key.to_string(),
+                        merge_object_property(properties_a.get(*key), properties_b.get(*key)),
+                    )
+                })
+                .collect();
+            NodeType::Object {
+                properties: merged_properties,
+            }
+        }
+        (NodeType::Any(mut xs), NodeType::Any(ys)) => {
+            let merged_types = ys.into_iter().fold(xs, |mut acc, y| {
+                if acc.contains(&y) {
+                    acc
+                } else {
+                    acc.push(y);
+                    acc
+                }
+            });
+            Any(merged_types)
+        }
+        (a @ NodeType::Any(_), b) | (b, a @ NodeType::Any(_)) => {
+            merge_node_type(a, NodeType::Any(vec![b]))
+        }
+        (a, b) => merge_node_type(NodeType::Any(vec![a]), NodeType::Any(vec![b])),
+    }
+}
+
+fn merge_object_property(a: Option<&ObjectProperty>, b: Option<&ObjectProperty>) -> ObjectProperty {
+    match (a, b) {
+        (Some(a), None) => ObjectProperty {
+            required: false,
+            ..a.clone()
+        },
+        (None, Some(b)) => ObjectProperty {
+            required: false,
+            ..b.clone()
+        },
+        (Some(a), Some(b)) => ObjectProperty {
+            required: a.required && b.required,
+            node_type: merge_node_type(a.clone().node_type, b.clone().node_type),
+        },
+        (None, None) => unreachable!(),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use maplit::hashmap;
+
+    use crate::merge::{merge_hypothesis, merge_node_type};
+    use crate::ObjectProperty;
+    use crate::{NodeType, SchemaHypothesis};
+
+    #[test]
+    fn test_merge_string() {
+        let a = SchemaHypothesis {
+            root: NodeType::String,
+        };
+        let b = SchemaHypothesis {
+            root: NodeType::String,
+        };
+
+        let actual = merge_hypothesis(a, b);
+
+        assert_eq!(
+            actual,
+            SchemaHypothesis {
+                root: NodeType::String
+            }
+        )
+    }
+
+    #[test]
+    fn test_merge_object_additional_property_b() {
+        let a = SchemaHypothesis {
+            root: NodeType::Object {
+                properties: hashmap! {
+                    String::from("id") => ObjectProperty { required: true, node_type: NodeType::String }
+                },
+            },
+        };
+
+        let b = SchemaHypothesis {
+            root: NodeType::Object {
+                properties: hashmap! {
+                    String::from("id") => ObjectProperty { required: true, node_type: NodeType::String },
+                    String::from("name") => ObjectProperty { required: true, node_type: NodeType::String }
+                },
+            },
+        };
+
+        let actual = merge_hypothesis(a, b);
+        let expected = SchemaHypothesis {
+            root: NodeType::Object {
+                properties: hashmap! {
+                    String::from("id") => ObjectProperty { required: true, node_type: NodeType::String },
+                    String::from("name") => ObjectProperty { required: false, node_type: NodeType::String }
+                },
+            },
+        };
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_merge_object_property_missing_in_b() {
+        let a = SchemaHypothesis {
+            root: NodeType::Object {
+                properties: hashmap! {
+                    String::from("id") => ObjectProperty { required: true, node_type: NodeType::String },
+                    String::from("name") => ObjectProperty { required: true, node_type: NodeType::String }
+                },
+            },
+        };
+
+        let b = SchemaHypothesis {
+            root: NodeType::Object {
+                properties: hashmap! {
+                    String::from("id") => ObjectProperty { required: true, node_type: NodeType::String },
+                },
+            },
+        };
+
+        let actual = merge_hypothesis(a, b);
+        let expected = SchemaHypothesis {
+            root: NodeType::Object {
+                properties: hashmap! {
+                    String::from("id") => ObjectProperty { required: true, node_type: NodeType::String },
+                    String::from("name") => ObjectProperty { required: false, node_type: NodeType::String }
+                },
+            },
+        };
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_merge_different_types() {
+        let a = NodeType::String;
+        let b = NodeType::Integer;
+
+        let actual = merge_node_type(a, b);
+
+        assert_eq!(
+            actual,
+            NodeType::Any(vec![NodeType::String, NodeType::Integer])
+        )
+    }
+
+    #[test]
+    fn test_merge_any_and_type() {
+        let a = NodeType::Any(vec![NodeType::Integer]);
+        let b = NodeType::String;
+
+        let actual = merge_node_type(a, b);
+
+        assert_eq!(
+            actual,
+            NodeType::Any(vec![NodeType::Integer, NodeType::String])
+        )
+    }
+
+    #[test]
+    fn test_merge_type_and_any() {
+        let a = NodeType::String;
+        let b = NodeType::Any(vec![NodeType::Integer]);
+
+        let actual = merge_node_type(a, b);
+
+        assert_eq!(
+            actual,
+            NodeType::Any(vec![NodeType::Integer, NodeType::String])
+        )
+    }
+
+    #[test]
+    fn test_merge_existing_type_and_any() {
+        let a = NodeType::Any(vec![NodeType::String, NodeType::Integer]);
+        let b = NodeType::String;
+
+        let actual = merge_node_type(a, b);
+
+        assert_eq!(
+            actual,
+            NodeType::Any(vec![NodeType::String, NodeType::Integer])
+        )
+    }
+}
