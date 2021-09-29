b14a70c9e0466bbcf0b7aa2d135bada4674dd19d
Elmar Athmer <elmar@athmer.org>
2021-09-29 17:18:04 +0200

    fix linter violation

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,5 +1,5 @@
 use backend::SchemaHypothesis;
-use renderer::{render_debug, render_schema};
+use renderer::render_schema;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let stdin = std::io::stdin();
