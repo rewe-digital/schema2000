1cb847f1e019d9cb100a7b14b271fbba75471e4e
lukas prediger <lukas.prediger@rewe-digital.com>
2021-09-29 11:40:04 +0200

    render the json schema to pretty printed string

--- renderer/src/lib.rs
+++ renderer/src/lib.rs
@@ -1,13 +1,15 @@
 mod json_schema_renderer;
 
+use crate::json_schema_renderer::render_json_schema;
 use backend::SchemaHypothesis;
+use serde_json::to_string_pretty;
 
 pub fn render_debug(schema: &SchemaHypothesis) -> String {
     format!("{:#?}", schema)
 }
 
-pub fn render_schema(_schema: &SchemaHypothesis) -> String {
-    todo!()
+pub fn render_schema(schema: &SchemaHypothesis) -> String {
+    to_string_pretty(&render_json_schema(schema)).unwrap()
 }
 
 pub fn render_typescript(_schema: &SchemaHypothesis) -> String {
