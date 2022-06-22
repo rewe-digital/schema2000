3f2c00f98e52c76272cabf36633ebc7b2abdc72f
Ewaryst.Schulz <ewaryst.schulz@rewe-digital.com>
2022-06-22 10:54:44 +0200

    mob next [ci-skip]

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -20,31 +20,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut hypothesises: HashMap<String, SchemaHypothesis> = HashMap::new();
 
     for json_document in iterator {
-        let new_hypo = schema2000::generate_hypothesis(&json_document?);
+        let document = &json_document?;
+        let discriminator_key = &extract_discriminator(document);
+        let new_hypo = schema2000::generate_hypothesis(extract_payload(document));
 
-        if hypothesises.contains_key(MAGIC_KEY) {
-            let current = hypothesises.get(MAGIC_KEY).unwrap().clone();
+        if hypothesises.contains_key(discriminator_key) {
+            let current = hypothesises.get(discriminator_key).unwrap().clone();
             let merged_hypo = schema2000::merge_hypothesis(current, new_hypo);
-            hypothesises.insert(MAGIC_KEY.to_string(), merged_hypo);
+            hypothesises.insert(discriminator_key.to_string(), merged_hypo);
         } else {
-            hypothesises.insert(MAGIC_KEY.to_string(), new_hypo);
+            hypothesises.insert(discriminator_key.to_string(), new_hypo);
         }
     }
 
-    let result = render_schema(hypothesises.get(MAGIC_KEY).unwrap());
-
-    println!("{}", result);
+    hypothesises.iter().for_each(|(discriminator_key, hypo)| {
+        let result = render_schema(hypo);
+        println!("{}", result);
+    });
 
     Ok(())
 }
 
-fn extract_payload(document: &Value) -> Value {
-    document
-        .as_object()
-        .unwrap()
-        .get("payload")
-        .unwrap()
-        .clone()
+fn extract_payload(document: &Value) -> &Value {
+    document.as_object().unwrap().get("payload").unwrap()
 }
 
 fn extract_discriminator(document: &Value) -> String {
