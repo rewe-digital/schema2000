2479ff7e4139cf48ea36e601c0e173dbd7b04bc9
lukas prediger <lukas@predigerweb.de>
2022-06-22 11:06:30 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
+use std::io::StdinLock;
 use std::ops::Deref;
 
 use clap::Parser;
-use serde_json::Value;
+use serde_json::de::IoRead;
+use serde_json::{StreamDeserializer, Value};
 
-use schema2000::{render_schema, SchemaHypothesis};
-
-const MAGIC_KEY: &str = "hypothesis";
+use schema2000::{generate_hypothesis_from_jsons, render_schema, SchemaHypothesis};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _args = Args::parse();
@@ -15,45 +15,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let stdin = stdin.lock();
 
     let deserializer = serde_json::Deserializer::from_reader(stdin);
-    let iterator = deserializer.into_iter::<Value>();
+    let iterator: StreamDeserializer<IoRead<StdinLock>, Value> = deserializer.into_iter::<Value>();
 
-    let mut hypothesises: HashMap<String, SchemaHypothesis> = HashMap::new();
+    let hypothesises = generate_hypothesis_from_jsons(iterator.collect())?;
 
-    for json_document in iterator {
-        let document = &json_document?;
-        let discriminator_key = &extract_discriminator(document);
-        let new_hypo = schema2000::generate_hypothesis(extract_payload(document));
-
-        if hypothesises.contains_key(discriminator_key) {
-            let current = hypothesises.get(discriminator_key).unwrap().clone();
-            let merged_hypo = schema2000::merge_hypothesis(current, new_hypo);
-            hypothesises.insert(discriminator_key.to_string(), merged_hypo);
-        } else {
-            hypothesises.insert(discriminator_key.to_string(), new_hypo);
-        }
-    }
-
-    hypothesises.iter().for_each(|(discriminator_key, hypo)| {
+    hypothesises.values().for_each(|hypo| {
         let result = render_schema(hypo);
         println!("{}", result);
     });
 
     Ok(())
-}
-
-fn extract_payload(document: &Value) -> &Value {
-    document.as_object().unwrap().get("payload").unwrap()
-}
-
-fn extract_discriminator(document: &Value) -> String {
-    document
-        .as_object()
-        .unwrap()
-        .get("discriminator")
-        .unwrap()
-        .as_str()
-        .unwrap()
-        .to_string()
 }
 
 #[derive(Parser, Debug)]
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,8 @@
 #![allow(clippy::module_name_repetitions)]
 
+use serde_json::Value;
+use std::collections::HashMap;
+
 pub use generate::generate_hypothesis;
 pub use merge::merge_hypothesis;
 pub use model::SchemaHypothesis;
@@ -10,3 +13,39 @@ mod merge;
 pub mod model;
 mod renderer;
 mod utils;
+
+pub fn generate_hypothesis_from_jsons(
+    json_documents: Vec<serde_json::Result<Value>>,
+) -> Result<HashMap<String, SchemaHypothesis>, Box<dyn std::error::Error>> {
+    let mut hypothesises: HashMap<String, SchemaHypothesis> = HashMap::new();
+
+    for json_document in json_documents {
+        let document = &json_document?;
+        let discriminator_key = &extract_discriminator(document);
+        let new_hypo = generate_hypothesis(extract_payload(document));
+
+        if hypothesises.contains_key(discriminator_key) {
+            let current = hypothesises.get(discriminator_key).unwrap().clone();
+            let merged_hypo = merge_hypothesis(current, new_hypo);
+            hypothesises.insert(discriminator_key.to_string(), merged_hypo);
+        } else {
+            hypothesises.insert(discriminator_key.to_string(), new_hypo);
+        }
+    }
+    Ok(hypothesises)
+}
+
+fn extract_payload(document: &Value) -> &Value {
+    document.as_object().unwrap().get("payload").unwrap()
+}
+
+fn extract_discriminator(document: &Value) -> String {
+    document
+        .as_object()
+        .unwrap()
+        .get("discriminator")
+        .unwrap()
+        .as_str()
+        .unwrap()
+        .to_string()
+}
